ac5cc8c25b062d49626f38d63adb3e81f70521a4
claudiuionescu08 <75946263+claudiuionescu08@users.noreply.github.com>
2023-03-06 14:49:59 +0200

    Update Main.java

--- Seminar 3/src/Main.java
+++ Seminar 3/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user = new User("John");
+        PaymentManager paymentManager = new PaymentManager();
+        paymentManager.doPayment(user);
     }
 }
