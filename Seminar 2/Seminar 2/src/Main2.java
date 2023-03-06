ea5cde1b0cc8480fb0c4b42e7bf177e4d3cf9f59
claudiuionescu08 <75946263+claudiuionescu08@users.noreply.github.com>
2023-03-06 14:46:50 +0200

    Delete Seminar 2/Seminar 2 directory

--- Seminar 2/Seminar 2/src/Main2.java
+++
@@ -1,7 +0,0 @@
-public class Main2 {
-    public static void main(String[] args) {
-        User user = new User("John");
-        PaymentManager paymentManager = new PaymentManager();
-        paymentManager.doPayment(user);
-    }
-}
--- Seminar 2/Seminar 2/src/PaymentManager.java
+++
@@ -1,6 +0,0 @@
-public class PaymentManager {
-    public void doPayment(User user) {
-        // implementarea metodei de efectuare a plății pentru un anumit utilizator
-        // ...
-    }
-}
