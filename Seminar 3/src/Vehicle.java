adb70056916253c5ca9e12bab517a01e8a67361a
claudiuionescu08 <75946263+claudiuionescu08@users.noreply.github.com>
2023-03-06 14:50:40 +0200

    Update Vehicle.java

--- Seminar 3/src/Vehicle.java
+++ Seminar 3/src/Vehicle.java
@@ -1,23 +1,22 @@
 public class Vehicle {
+    public double calculateValue(double a){
+        return a;
+    }
+
+    public double getValue(double a){
+        return a;
+    }
+
+}
+
+class Car extends Vehicle{
     public double calculateValue() {
-        return 0;
+        return this.getValue(calculateValue()) * 0.8;
     }
 }
-public class Car extends Vehicle{
-    public double calculateValue(){
-        return this.getValue()*0.8;
+
+class Truck extends Vehicle{
+    public double calculateValue() {
+        return this.getValue(calculateValue()) * 0.9;
     }
-
-    private double getValue() {
-    }
-
-    public class Truck extends Vehicle{
-        public double calculateValue(){
-            return this.getValue()*0.9;
-        }
-
-        private double getValue() {
-        }
-
-    };
-
+}
